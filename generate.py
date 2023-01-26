2039f2e7f27d13eeaf1b83ca5294e5ccc91fa504
marajadeskywalker <maxwell.heath@uvm.edu>
2023-01-26 12:24:54 -0500

    updated generate.py

--- generate.py
+++ generate.py
@@ -1,28 +1,17 @@
 import pyrosim.pyrosim as pyrosim
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[0, 3, 0.5], size=[1, 1, 1])
+    pyrosim.End()
 
-x = 2
-y = 2
-z = 0
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[-0.5, 0, 1])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[0.5, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[-0.5, 0, -0.5], size=[1,1,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[0.5, 0, -0.5], size=[1,1,1])
+    pyrosim.End()
 
-length = 1
-width = 1
-height = 1
-
-pyrosim.Start_SDF("boxes.sdf")
-for i in range(5):
-    for j in range(5):
-        for k in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length,width,height])
-            z += 1.1
-            length *= 0.9
-            width *= 0.9
-            height *= 0.9
-        z = 0
-        length = 1
-        width = 1
-        height = 1
-        y -= 1
-    x -= 1
-    y = 2
-
-pyrosim.End()
+Create_World()
+Create_Robot()

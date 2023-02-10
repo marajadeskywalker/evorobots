b79d31a38f769cab810cd4a62a7a7e1665096a22
marajadeskywalker <maxwell.heath@uvm.edu>
2023-02-10 15:24:27 -0500

    finished synapses assignment

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -23,8 +23,16 @@ class NEURON:
     def Update_Sensor_Neuron(self):
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
 
-    def Update_Hidden_Or_Motor_Neuron(self):
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         self.Set_Value(0.0)
+        for key in synapses:
+            if(key[1] == self.Get_Name()):
+                self.Allow_Presynaptic_Neuron_To_Influence_Me(synapses[key].Get_Weight(), neurons[key[0]].Get_Value())
+        self.Threshold()
+
+    def Allow_Presynaptic_Neuron_To_Influence_Me(self, synapse_weight: float, presynaptic_value: float):
+        self.Add_To_Value(synapse_weight*presynaptic_value)
+
 
     def Add_To_Value( self, value ):
 
